33fb4067f61e5c6c6ce19aea9d16475859c0b696
anaskhan28 <khananasatique@gmail.com>
2022-11-24 10:56:42 +0530

    fixed house listing error

--- src/components/House.js
+++ src/components/House.js
@@ -9,11 +9,39 @@ const House = ({house}) => {
   hover:shadow-2xl transition'>
     <img className="mb-8" src={image}alt=''/>
     <div className='mb-4 flex gap-x-2 text-sm'>
-      <div className=''>{type}</div>
-      <div>{country}</div>
-      <div>{address}</div>
+      <div className='bg-green-500 rounded-full
+      text-white px-3'>{type}</div>
+      <div className='bg-violet-500 rounded-full 
+        px-3  text-white'>{country}</div>
+      
     </div>
-  </div>;
+    <div className='text-lg font-semibold max-w-[260px]'>{address}</div>
+    <div className='flex gap-x-4 my-4'>
+      <div className='flex items-center text-gray-600
+      gap-1'>
+        <div className='text-[20px]'>
+          <BiBed/>
+          </div>
+        <div>{bedrooms}</div>
+      </div>
+      <div className='flex items-center text-gray-600
+      gap-1'>
+        <div className='text-[20px]'>
+          <BiBath/>
+          </div>
+        <div>{bathrooms}</div>
+      </div>
+      <div className='flex items-center text-gray-600
+      gap-1'>
+        <div className='text-[20px]'>
+          <BiArea/>
+          </div>
+        <div>{surface}</div>
+      </div>
+    </div>
+    <div className='text-lg font-semibold text-violet-600
+    mb-4'>{price}</div>
+  </div>
 };
 
 export default House;
